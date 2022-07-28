05eecae4482ebb8e9cd41d31a233d844ec3e92b3
TrinityWeaver <sebastian.marynicz@gmail.com>
2022-07-28 23:28:48 +0100

    Ttitle change

--- src/views/all-home-version/HomeDarkAnimation.jsx
+++ src/views/all-home-version/HomeDarkAnimation.jsx
@@ -13,7 +13,7 @@ import useDocumentTitle from "../../components/useDocumentTitle";
 
 const HomeOne = () => {
   useDocumentTitle(
-    "Nairo || Theme Dark Animation React Personal Portfolio Template"
+    "Sebastian's Resume"
   );
   return (
     <div className="main-left theme-dark">
