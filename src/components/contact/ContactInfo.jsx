a92e29197ba9453db7f93359598381315ef272bb
TrinityWeaver <sebastian.marynicz@gmail.com>
2022-08-01 10:36:43 +0100

    ContactInfo done

--- src/components/contact/ContactInfo.jsx
+++ src/components/contact/ContactInfo.jsx
@@ -1,40 +1,40 @@
 import React from "react";
 
 const ContactInfo = () => {
-  return (
-    <>
-      <div className="contact-info">
-        <h4>What’s your story? Get in touch</h4>
-        <p>
-          Always available for freelancing if the right project comes along,
-          Feel free to contact me.
-        </p>
+	return (
+		<>
+			<div className="contact-info">
+				<h4>What's your story? Get in touch</h4>
+				<p>
+					Always available for freelancing if the right project comes along, I
+					am also open to job offers. Feel free to contact me.
+				</p>
 
-        <ul>
-          <li className="media">
-            <i className="icon icon-map"></i>
-            <span className="media-body">
-              123 Stree New York City , United States Of America 750065.
-            </span>
-          </li>
-          {/* End li */}
+				<ul>
+					<li className="media">
+						<i className="icon icon-map"></i>
+						<span className="media-body">
+							Brick Lane, London, E1 6PU, United Kingdom.
+						</span>
+					</li>
+					{/* End li */}
 
-          <li className="media">
-            <i className="icon icon-envelope"></i>
-            <span className="media-body">support@domain.com</span>
-          </li>
-          {/* End li */}
+					<li className="media">
+						<i className="icon icon-envelope"></i>
+						<span className="media-body">sebastian.marynicz@gmail.com</span>
+					</li>
+					{/* End li */}
 
-          <li className="media">
-            <i className="icon icon-phone"></i>
-            <span className="media-body">+044 9696 9696 3636</span>
-          </li>
-          {/* End li */}
-        </ul>
-      </div>
-      {/* End .contact-info */}
-    </>
-  );
+					<li className="media">
+						<i className="icon icon-phone"></i>
+						<span className="media-body">+044 7936657257</span>
+					</li>
+					{/* End li */}
+				</ul>
+			</div>
+			{/* End .contact-info */}
+		</>
+	);
 };
 
 export default ContactInfo;
